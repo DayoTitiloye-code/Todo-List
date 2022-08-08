189d63b6a859924bb84f3f807a12152c8a2fb1bc
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-08-08 16:03:34 +0100

    Updated todo input form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
-  return <div>Hello World</div>
-}
+import React, { Component } from 'react'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import uuid from 'uuid'
 
-export default App
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          {/*div 10 columns wide, places div in centre, medium screen = 8 columns wide + padding*/}
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput />
+            <TodoList />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBook } from '@fortawesome/free-solid-svg-icons'
 export default class TodoInput extends Component {
   render() {
-    return <div>TodoInput</div>
+    return (
+      <div className="card card-body my-3">
+        <form>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <FontAwesomeIcon icon={faBook} />
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Add a Todo Item!"
+            />
+          </div>
+          <button type="submit" className="btn btn-block btn-primary mt-3">
+            Add Item
+          </button>
+        </form>
+      </div>
+    )
   }
 }
