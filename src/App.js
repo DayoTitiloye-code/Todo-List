09c6f169c2446c4aabd300b3f028d184e40c7d18
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-08-10 14:11:00 +0100

    Created methods to handle input change+submission

--- src/App.js
+++ src/App.js
@@ -2,9 +2,43 @@ import React, { Component } from 'react'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import uuid from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 export default class App extends Component {
+  state = {
+    items: [],
+    id: uuidv4(),
+    itemName: '',
+    editItem: false,
+  }
+
+  inputChanges = (e) => {
+    this.setState({
+      itemName: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    //prevents page refresh
+    e.preventDefault()
+
+    const newItem = {
+      id: this.state.id,
+      itemName: this.state.itemName,
+    }
+
+    console.log(newItem)
+    //copy array into new array using spread operator
+    const updatedItems = [...this.state.items, newItem]
+
+    this.setState({
+      items: updatedItems,
+      itemName: '',
+      id: uuidv4(),
+      editItem: false,
+    })
+  }
+
   render() {
     return (
       <div className="container">
@@ -12,7 +46,11 @@ export default class App extends Component {
           {/*div 10 columns wide, places div in centre, medium screen = 8 columns wide + padding*/}
           <div className="col-10 mx-auto col-md-8 mt-4">
             <h3 className="text-capitalize text-center">todo input</h3>
-            <TodoInput />
+            <TodoInput
+              item={this.state.itemName}
+              inputChanges={this.inputChanges}
+              handleSubmit={this.handleSubmit}
+            />
             <TodoList />
           </div>
         </div>
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -3,15 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBook } from '@fortawesome/free-solid-svg-icons'
 export default class TodoInput extends Component {
   render() {
+    //destructuring - used to select only part of the object
+    const { itemName, inputChanges, handleSubmit } = this.props
     return (
       <div className="card card-body my-3">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <div className="input-group-prepend"></div>
             <input
               type="text"
               className="form-control"
               placeholder="Add a Todo Item!"
+              value={itemName}
+              onChange={inputChanges}
             />
           </div>
           <button type="submit" className="btn btn-primary btn-block  mt-3">
